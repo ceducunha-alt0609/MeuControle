5559b390ef6bb4df4b00c9fdfb8b0d45da5025c4
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 16:02:39 -0300

    Atualiza bootstrap V3.48 para indicador de refresh

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.47 */
+/* Meu Controle - Service Worker bootstrap V3.48: indicador Atualizando nos refreshes sensiveis */
 importScripts('./sw-core-v315.js');
