6693d5c819f8902451820cc3c25dac1e5d0ef953
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 12:12:51 -0300

    chore(sw): renovar acabamento dark da Ajuda

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle — Service Worker bootstrap V3.28: renova acabamento dark do Aplicativo */
+/* Meu Controle — Service Worker bootstrap V3.29: renova acabamento dark da Ajuda */
 importScripts('./sw-core-v315.js');
