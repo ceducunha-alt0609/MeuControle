847060d8450ac77d74f8e1163c891d30f4c1bb02
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 19:26:56 -0300

    Ciclo 10/10: atualiza bootstrap para refinamentos do Painel

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.52: estabilização anti-flicker do Calendário desktop */
-importScripts('./sw-core-v316.js?v=352');
+/* Meu Controle - Service Worker bootstrap V3.53: Ciclo 10/10 do Painel */
+importScripts('./sw-core-v316.js?v=353');
