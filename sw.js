0ac601c004c7530fb6b3d04dfb40164f868e28d4
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 19:17:43 -0300

    Atualiza bootstrap V3.52 para estabilização do calendário desktop

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.51: primeiro paint protegido no mobile e Calendário desktop */
-importScripts('./sw-core-v316.js?v=351');
+/* Meu Controle - Service Worker bootstrap V3.52: estabilização anti-flicker do Calendário desktop */
+importScripts('./sw-core-v316.js?v=352');
