ba96b97abc6c02f9c93316d7019bb3a637105436
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 11:12:49 -0300

    chore(sw): renovar arraste livre do painel de acoes

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle — Service Worker bootstrap V3.23: renova painel flutuante dark */
+/* Meu Controle — Service Worker bootstrap V3.24: renova arraste livre do painel de acoes */
 importScripts('./sw-core-v315.js');
