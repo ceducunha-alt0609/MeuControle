64604be605745436198281e87632c59ed87a66c8
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 21:22:33 -0300

    Atualiza cache para estado vazio dark

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.60: Ciclo 2 multiusuario local */
-importScripts('./sw-core-v320.js?v=360');
+/* Meu Controle - Service Worker bootstrap V3.61: estado vazio compatível com tema escuro */
+importScripts('./sw-core-v320.js?v=361');
