f63696cc9c4b50cce1d9ecf3e0b5283a9bbac59f
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 21:51:12 -0300

    Atualiza cache para estabilizar status da nuvem no mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.63: sincronizacao Firestore sem reload */
-importScripts('./sw-core-v321.js?v=363');
+/* Meu Controle - Service Worker bootstrap V3.64: status da nuvem mobile sem repintadas */
+importScripts('./sw-core-v321.js?v=364');
