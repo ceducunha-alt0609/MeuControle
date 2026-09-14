33aa48510873393946a6ffec7145bccdad07c8d9
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 22:46:45 -0300

    Força indicador remoto V1.05 e ordem fixa da topbar

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.70: indicador remoto v104 */
-importScripts('./sw-core-v324.js?v=370');
+/* Meu Controle - Service Worker bootstrap V3.71: ordem fixa Sino > Sync > Perfil */
+importScripts('./sw-core-v324.js?v=371');
