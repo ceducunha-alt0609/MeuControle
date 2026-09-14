dc7d2a4bb526b92c99263c5602be3951f355818f
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 21:56:09 -0300

    Forca motor unico de sincronizacao no mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.64: status da nuvem mobile sem repintadas */
-importScripts('./sw-core-v321.js?v=364');
+/* Meu Controle - Service Worker bootstrap V3.65: motor unico de sincronizacao */
+importScripts('./sw-core-v321.js?v=365');
