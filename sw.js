a88b4e17ebc3640d467e341ce1cd4b875588e7af
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 23:07:44 -0300

    Atualiza service worker para sync protegido e sino por evento

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.72: sino remoto binario e estavel */
-importScripts('./sw-core-v324.js?v=372');
+/* Meu Controle - Service Worker bootstrap V3.73: protege edicoes locais e sino por evento */
+importScripts('./sw-core-v324.js?v=373');
