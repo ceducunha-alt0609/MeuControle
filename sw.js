b5737922e6746e176feca12cc2fd0daba15a9ffe
AquaControl <c.edu.cunha@gmail.com>
2026-09-14 09:15:34 -0300

    Atualiza service worker apos remover botao criar no calendario mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.77: estado vazio diário sem duplicação */
-importScripts('./sw-core-v326.js?v=377');
+/* Meu Controle - Service Worker bootstrap V3.78: calendário mobile sem botão criar redundante */
+importScripts('./sw-core-v326.js?v=378');
