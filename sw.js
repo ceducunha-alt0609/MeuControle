448a005e4fe1cf780cc55ef39791da4d5e19b76b
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 22:43:16 -0300

    Força indicador remoto v104 no PC e mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.69: sino remoto deduplicado e reposicionado */
-importScripts('./sw-core-v324.js?v=369');
+/* Meu Controle - Service Worker bootstrap V3.70: indicador remoto v104 */
+importScripts('./sw-core-v324.js?v=370');
