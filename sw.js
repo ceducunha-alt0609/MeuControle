83b283a6bf7ca12c4dd6197a9e45654c8cf1e8f3
AquaControl <c.edu.cunha@gmail.com>
2026-09-13 22:54:23 -0300

    Atualiza service worker para indicador remoto v106

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.71: ordem fixa Sino > Sync > Perfil */
-importScripts('./sw-core-v324.js?v=371');
+/* Meu Controle - Service Worker bootstrap V3.72: sino remoto binario e estavel */
+importScripts('./sw-core-v324.js?v=372');
