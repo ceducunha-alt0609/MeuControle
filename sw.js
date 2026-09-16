573ffaaf5d99cde0b02f4cbe6a97d5f133cde0bc
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 19:23:34 -0300

    Atualiza correção de conflito do seletor do Calendário

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.20: seletor do Calendário integrado nativamente à competência */
-importScripts('./sw-core-v326.js?v=420');
+/* Meu Controle - Service Worker bootstrap V4.21: remove disputa externa no seletor do Calendário */
+importScripts('./sw-core-v326.js?v=421');
