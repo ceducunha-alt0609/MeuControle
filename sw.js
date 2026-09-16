21695727302100c9f9543d0caa5812117c1d518d
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 12:17:25 -0300

    Atualiza bootstrap para primeiro paint estável da Agenda

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.12: Agenda mobile liberada somente após restauração visual */
-importScripts('./sw-core-v326.js?v=412');
+/* Meu Controle - Service Worker bootstrap V4.13: primeiro paint estável da Agenda mobile */
+importScripts('./sw-core-v326.js?v=413');
