193c468a34c21c5fe59eee769b44b67577637e33
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 06:31:47 -0300

    Atualiza cache para geometria estável da Agenda

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.99: protege somente lista inferior durante montagem da Agenda */
-importScripts('./sw-core-v326.js?v=399');
+/* Meu Controle - Service Worker bootstrap V4.00: fixa geometria dos cards da Agenda mobile */
+importScripts('./sw-core-v326.js?v=400');
