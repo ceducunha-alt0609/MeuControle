f1a5a37587d56c22bd8d1ebb8756e0152f1b6056
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 19:46:59 -0300

    Atualiza refino premium da Pesquisa Global

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.24: Pesquisa Global fora da topbar e centralizada */
-importScripts('./sw-core-v326.js?v=424');
+/* Meu Controle - Service Worker bootstrap V4.25: Pesquisa Global compacta e confirmação premium */
+importScripts('./sw-core-v326.js?v=425');
