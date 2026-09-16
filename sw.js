bbe640073eb65d82ec30f660bcedceb7c7e2b1ae
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 00:28:18 -0300

    Atualiza cache para estrutura corrigida da pesquisa

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.85: resultados compactos e action sheet móvel */
-importScripts('./sw-core-v326.js?v=385');
+/* Meu Controle - Service Worker bootstrap V3.86: cards em grid e action sheet real */
+importScripts('./sw-core-v326.js?v=386');
