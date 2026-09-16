0a2c19637b80c465a404208378213d1e7746cb9d
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 05:51:40 -0300

    Atualiza cache para valores financeiros nos cards

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.96: reverte proteção ampla da Agenda mobile */
-importScripts('./sw-core-v326.js?v=396');
+/* Meu Controle - Service Worker bootstrap V3.97: restaura valores financeiros nos cards */
+importScripts('./sw-core-v326.js?v=397');
