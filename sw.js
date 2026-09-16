0bbb256fe21b86d5218be51f8def4f1cfadf27a7
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 06:47:21 -0300

    Atualiza cache para render único da Agenda

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.00: fixa geometria dos cards da Agenda mobile */
-importScripts('./sw-core-v326.js?v=400');
+/* Meu Controle - Service Worker bootstrap V4.01: elimina segundo render tardio da Agenda mobile */
+importScripts('./sw-core-v326.js?v=401');
