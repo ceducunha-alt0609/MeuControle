94f2b28a6184f414b9899d553efc6ae5ff8ec01f
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 19:30:49 -0300

    Atualiza seletor real do Calendário

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.21: remove disputa externa no seletor do Calendário */
-importScripts('./sw-core-v326.js?v=421');
+/* Meu Controle - Service Worker bootstrap V4.22: sincroniza o seletor real do calendário compacto */
+importScripts('./sw-core-v326.js?v=422');
