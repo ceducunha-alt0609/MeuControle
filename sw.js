f3818c7b099d297f06bb07742f06e88e999ad634
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 01:12:05 -0300

    Força atualização após reverter tentativa de ocultação

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V3.91: valores integrados aos cards de resultados */
-importScripts('./sw-core-v326.js?v=391');
+/* Meu Controle - Service Worker bootstrap V3.93: restaura renderização estável de lançamentos */
+importScripts('./sw-core-v326.js?v=393');
