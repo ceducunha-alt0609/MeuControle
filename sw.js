d0de2be7ff195463ed820f14823527eb495d669b
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 08:33:11 -0300

    Atualiza cache para voltar nativo em Consultar lançamentos

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.07: integra Novo lançamento ao voltar nativo do Android */
-importScripts('./sw-core-v326.js?v=407');
+/* Meu Controle - Service Worker bootstrap V4.08: integra Consultar lançamentos ao voltar nativo do Android */
+importScripts('./sw-core-v326.js?v=408');
