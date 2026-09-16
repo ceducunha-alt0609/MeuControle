5504d3e28513aa442bbb8287e4c5714a2d5e9b58
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 17:36:13 -0300

    Força atualização da correção mensal de pendentes

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.14: corrige despesas pendentes no desktop */
-importScripts('./sw-core-v326.js?v=414');
+/* Meu Controle - Service Worker bootstrap V4.15: atualiza pendentes ao navegar entre meses */
+importScripts('./sw-core-v326.js?v=415');
