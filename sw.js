b7e0178dacaef176e26511beda08e77bb6ec778c
AquaControl <c.edu.cunha@gmail.com>
2026-09-16 06:57:38 -0300

    Atualiza cache para cards mobile com valor lateral

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.01: elimina segundo render tardio da Agenda mobile */
-importScripts('./sw-core-v326.js?v=401');
+/* Meu Controle - Service Worker bootstrap V4.02: cards mobile uniformes com valor lateral */
+importScripts('./sw-core-v326.js?v=402');
