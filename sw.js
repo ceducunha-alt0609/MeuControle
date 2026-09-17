44c724b40fd337e0361318275fc458b8ec0237d3
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 08:36:16 -0300

    Atualiza cache do detalhe de recebimentos mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.32: remove dica redundante dos cards no Painel mobile */
-importScripts('./sw-core-v326.js?v=432');
+/* Meu Controle - Service Worker bootstrap V4.33: refina detalhes de recebimentos no Painel mobile */
+importScripts('./sw-core-v326.js?v=433');
