e251a0092659f123afbebaae1b0d242471c784ba
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 09:48:12 -0300

    Atualiza cache do formulário mobile compacto

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.38: recebimentos enxutos com data, hora e valor */
-importScripts('./sw-core-v326.js?v=438');
+/* Meu Controle - Service Worker bootstrap V4.39: observações compactas no novo lançamento mobile */
+importScripts('./sw-core-v326.js?v=439');
