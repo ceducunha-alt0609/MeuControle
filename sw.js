990b28c25a60f4ae3281ca43381ca21e61b6869b
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 12:32:51 -0300

    SW V4.47: despesas vermelhas também no mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.46: card Despesas com identidade visual vermelha */
-importScripts('./sw-core-v326.js?v=446');
+/* Meu Controle - Service Worker bootstrap V4.47: card Despesas com nuance vermelha também no mobile */
+importScripts('./sw-core-v326.js?v=447');
