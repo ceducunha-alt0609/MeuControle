1bb5d1177395e99fef5c0073358bcb3cc18828fc
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 12:41:57 -0300

    SW V4.48: Central Hoje desktop limitada a dois itens

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.47: card Despesas com nuance vermelha também no mobile */
-importScripts('./sw-core-v326.js?v=447');
+/* Meu Controle - Service Worker bootstrap V4.48: Central Hoje desktop mostra 2 itens por coluna e excedentes nos detalhes */
+importScripts('./sw-core-v326.js?v=448');
