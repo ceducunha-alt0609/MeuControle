2d989599eafbeb18e171f8c8ce7da777a2a28b6f
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 09:28:52 -0300

    Atualiza cache dos recebimentos enxutos mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.37: estrutura recebimentos em duas colunas como despesas */
-importScripts('./sw-core-v326.js?v=437');
+/* Meu Controle - Service Worker bootstrap V4.38: recebimentos enxutos com data, hora e valor */
+importScripts('./sw-core-v326.js?v=438');
