1e99550837bdc6c552ec93a8fc695590dc708787
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 08:19:45 -0300

    Atualiza cache do card A receber mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.28: hierarquia visual da competência no calendário */
-importScripts('./sw-core-v326.js?v=428');
+/* Meu Controle - Service Worker bootstrap V4.29: card A receber no mês no Painel mobile */
+importScripts('./sw-core-v326.js?v=429');
