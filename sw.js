88135aac3ebee41417beb3e8b19556929ac04f7c
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 10:01:34 -0300

    Atualiza cache para observações mais enxutas no mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.39: observações compactas no novo lançamento mobile */
-importScripts('./sw-core-v326.js?v=439');
+/* Meu Controle - Service Worker bootstrap V4.40: observações ainda mais compactas no novo lançamento mobile */
+importScripts('./sw-core-v326.js?v=440');
