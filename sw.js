9a78d037bb2e038401f73fa36cde8b82fb46bad9
AquaControl <c.edu.cunha@gmail.com>
2026-09-17 11:47:33 -0300

    SW V4.45: corrige legenda de previstos no Painel

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.44: cards do Painel desktop obedecem à competência selecionada */
-importScripts('./sw-core-v326.js?v=444');
+/* Meu Controle - Service Worker bootstrap V4.45: corrige legenda de previstos no Painel desktop */
+importScripts('./sw-core-v326.js?v=445');
