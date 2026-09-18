1e3000b8f79cfc2cc11786b3817ecd36f1d0c091
AquaControl <c.edu.cunha@gmail.com>
2026-09-18 00:13:19 -0300

    Atualiza cache para tema escuro dos painéis mobile

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.48: Central Hoje desktop mostra 2 itens por coluna e excedentes nos detalhes */
-importScripts('./sw-core-v326.js?v=448');
+/* Meu Controle - Service Worker bootstrap V4.49: action sheets mobile compatíveis com tema escuro */
+importScripts('./sw-core-v326.js?v=449');
