6d0bd749d455166c0d355e0f8b5d225fe9c5a6af
AquaControl <c.edu.cunha@gmail.com>
2026-09-19 00:11:39 -0300

    Atualiza bootstrap para limpeza do calendário desktop

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.62: restaura calendário desktop estável */
-importScripts('./sw-core-v326.js?v=462');
+/* Meu Controle - Service Worker bootstrap V4.63: limpa testes de layout do calendário desktop */
+importScripts('./sw-core-v326.js?v=463');
