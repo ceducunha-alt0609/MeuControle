3fb00bbed31cfbef6a5928525f11260cb00edaac
AquaControl <c.edu.cunha@gmail.com>
2026-09-19 00:03:21 -0300

    Atualiza bootstrap para hook central do calendário desktop

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.60: valor integrado ao renderizador do calendário desktop */
-importScripts('./sw-core-v326.js?v=460');
+/* Meu Controle - Service Worker bootstrap V4.61: hook central dos cards do calendário desktop */
+importScripts('./sw-core-v326.js?v=461');
