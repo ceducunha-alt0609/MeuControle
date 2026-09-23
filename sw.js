73229dd586141932a4bb2a07b8543512adb87b0f
AquaControl <c.edu.cunha@gmail.com>
2026-09-22 22:40:13 -0300

    Renova versão do PWA após seletor de unidade

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.69: limpa testes de layout do calendário desktop */
-importScripts('./sw-core-v332.js?v=469');
+/* Meu Controle - Service Worker bootstrap V4.70: limpa testes de layout do calendário desktop */
+importScripts('./sw-core-v332.js?v=470');
