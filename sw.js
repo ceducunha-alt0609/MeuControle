f519f91b9c99833b019dce3b37b473a50a6740bf
AquaControl <c.edu.cunha@gmail.com>
2026-09-22 23:20:33 -0300

    Atualiza service worker para cache v3.33

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.71: limpa testes de layout do calendário desktop */
-importScripts('./sw-core-v332.js?v=471');
+/* Meu Controle - Service Worker bootstrap V4.72: limpa testes de layout do calendário desktop */
+importScripts('./sw-core-v332.js?v=472');
