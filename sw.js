3a75a78732f10600dcee580282c38ffd818eb05f
AquaControl <c.edu.cunha@gmail.com>
2026-09-22 22:47:37 -0300

    Renova PWA após centralizar seletor

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.70: limpa testes de layout do calendário desktop */
-importScripts('./sw-core-v332.js?v=470');
+/* Meu Controle - Service Worker bootstrap V4.71: limpa testes de layout do calendário desktop */
+importScripts('./sw-core-v332.js?v=471');
