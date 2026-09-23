57b07d60a6051d50eaef7a87993409d3d57e5abe
AquaControl <c.edu.cunha@gmail.com>
2026-09-22 22:00:19 -0300

    Ativa cache v3.28

--- sw.js
+++ sw.js
@@ -1,2 +1,2 @@
-/* Meu Controle - Service Worker bootstrap V4.64: limpa testes de layout do calendário desktop */
-importScripts('./sw-core-v327.js?v=464');
+/* Meu Controle - Service Worker bootstrap V4.65: limpa testes de layout do calendário desktop */
+importScripts('./sw-core-v328.js?v=465');
